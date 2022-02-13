1b4743a0ebc6ac82ad445da3b402cfd9436468ef
Alexamakans <fougner.alexander@gmail.com>
2022-02-13 21:32:13 +0100

    Added TabSelection script

---
+++ Assets/Scripts/HostOrJoinScene/TabSelection.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TabSelection : MonoBehaviour
+{
+    private EventSystem system;
+
+    private void Start() {
+        system = EventSystem.current;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable target;
+
+            target = system.currentSelectedGameObject?.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (target != null) {
+                TMPro.TMP_InputField inputField = target.GetComponent<TMPro.TMP_InputField>();
+                if (inputField != null) {
+                    inputField.OnPointerClick(new PointerEventData(system));
+                }
+
+                system.SetSelectedGameObject(target.gameObject, new BaseEventData(system));
+            }
+        
+        }
+    }
+}
