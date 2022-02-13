64a16f0f7269604d4d12adfca6c1e0eeeba97371
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-13 10:26:11 +0100

    Changed step message

--- Assets/Scripts/HostOrJoinScene/HostOrJoinScript.cs
+++ Assets/Scripts/HostOrJoinScene/HostOrJoinScript.cs
@@ -7,12 +7,13 @@ public class HostOrJoinScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
