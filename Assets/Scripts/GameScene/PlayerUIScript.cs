22ff21e1c78f4a2afd98f37e3ef1bcc52109258f
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-26 14:49:57 +0100

    Added editing name by clicking name

--- Assets/Scripts/GameScene/PlayerUIScript.cs
+++ Assets/Scripts/GameScene/PlayerUIScript.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerUIScript : MonoBehaviour
 {
+    public string typeHost = "HOST";
+    public string typeContestant = "CONTESTANT";
+    public string formatOther = "<b>{0}</b>";
+    public string formatYou = "<b>{0}</b> <size=80%>(you)</size>";
+
     public TMP_Text textName;
+    public TMP_InputField fieldName;
     public TMP_Text textType;
     private PlayerScript previousPlayer;
 
@@ -22,20 +28,37 @@ public class PlayerUIScript : MonoBehaviour
         {
             previousPlayer.playerName.OnValueChanged -= OnPlayerNameChanged;
         }
+
         player.playerName.OnValueChanged += OnPlayerNameChanged;
 
+        fieldName.text =
         textName.text = player.playerName.Value.Value;
-        var type = player.IsOwnedByServer ? "HOST" : "CONTESTANT";
-        if (player.IsLocalPlayer)
-        {
-            type += " (you)";
-        }
-        textType.text = type;
+        fieldName.gameObject.SetActive(player.IsOwner);
+        textName.gameObject.SetActive(!player.IsOwner);
+
+        var type = player.IsOwnedByServer ? typeHost : typeContestant;
+        var format = player.IsLocalPlayer ? formatYou : formatOther;
+        textType.text = string.Format(format, type);
         previousPlayer = player;
     }
 
     private void OnPlayerNameChanged(FixedString128Bytes previousValue, FixedString128Bytes newValue)
     {
+        if (fieldName.isFocused)
+        {
+            return;
+        }
+        fieldName.text =
         textName.text = newValue.Value;
+    }
+
+    public void OnNameFieldInputChanged()
+    {
+        if (!previousPlayer
+            || previousPlayer.playerName.Value == fieldName.text)
+        {
+            return;
+        }
+        previousPlayer.SetPlayerNameServerRpc(fieldName.text);
     }
 }
